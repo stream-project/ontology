68f46a25352d875227067bbf1d9771c18042f846
Kurt Junghanns <tboonx@googlemail.com>
2020-08-04 11:22:21 +0200

    again trying to include file content into email body

--- interprete.sh
+++ interprete.sh
@@ -5,6 +5,10 @@ sh -c " cat ./oops_result.xml | xq -c '.[\"rdf:RDF\"] | .[\"rdf:Description\"] |
 sh -c " cat ./oops_result.xml | xq -c '.[\"rdf:RDF\"] | .[\"rdf:Description\"] |  map( select( [ .[\"oops:hasImportanceLevel\"] | .[\"#text\"] == \"Important\" ] | any )? )' | jq -c '.[] | (.[\"oops:hasNumberAffectedElements\"] | .[\"#text\"]), (.[\"oops:hasDescription\"] | .[\"#text\"])'" > important.txt
 sh -c " cat ./oops_result.xml | xq -c '.[\"rdf:RDF\"] | .[\"rdf:Description\"] |  map( select( [ .[\"oops:hasImportanceLevel\"] | .[\"#text\"] == \"Minor\" ] | any )? )' | jq -c '.[] | (.[\"oops:hasNumberAffectedElements\"] | .[\"#text\"]), (.[\"oops:hasDescription\"] | .[\"#text\"])'" > minor.txt
 
+# Put everything into one file
+echo "OOPS Summary: (always the occurrences amount and then the description) \n Critical: \n " `cat critical.txt` " \n  \n Important: \n " `cat important.txt` " \n  \n Minor: \n " `cat minor.txt` " \n  \n " > reports.txt
+echo "RDFUnit Summary: \n " `cat RDFUnit_errors.txt` >> reports.txt
+
 # Now read RDFUnit report
 lines=$(awk 'END{print NR}' RDFUnit_errors.txt)
 
