6a24433c98ce97bc60a8ee52f754d573080af7ab
Kurt Junghanns <tboonx@googlemail.com>
2020-08-07 11:43:07 +0200

    reenable exit if errors and try to improve oops results visually

--- interprete.sh
+++ interprete.sh
@@ -3,7 +3,7 @@
 # First read OOPS results
 sh -c " cat ./oops_result.xml | xq -c '.[\"rdf:RDF\"] | .[\"rdf:Description\"] |  map( select( [ .[\"oops:hasImportanceLevel\"] | .[\"#text\"] == \"Critical\" ] | any )? )' | jq -c '.[] | (.[\"oops:hasNumberAffectedElements\"] | .[\"#text\"]), (.[\"oops:hasDescription\"] | .[\"#text\"])'" > critical.txt
 sh -c " cat ./oops_result.xml | xq -c '.[\"rdf:RDF\"] | .[\"rdf:Description\"] |  map( select( [ .[\"oops:hasImportanceLevel\"] | .[\"#text\"] == \"Important\" ] | any )? )' | jq -c '.[] | (.[\"oops:hasNumberAffectedElements\"] | .[\"#text\"]), (.[\"oops:hasDescription\"] | .[\"#text\"])'" > important.txt
-sh -c " cat ./oops_result.xml | xq -c '.[\"rdf:RDF\"] | .[\"rdf:Description\"] |  map( select( [ .[\"oops:hasImportanceLevel\"] | .[\"#text\"] == \"Minor\" ] | any )? )' | jq -c '.[] | (.[\"oops:hasNumberAffectedElements\"] | .[\"#text\"]), (.[\"oops:hasDescription\"] | .[\"#text\"])'" > minor.txt
+sh -c " cat ./oops_result.xml | xq -c '.[\"rdf:RDF\"] | .[\"rdf:Description\"] |  map( select( [ .[\"oops:hasImportanceLevel\"] | .[\"#text\"] == \"Minor\" ] | any )? )' | jq -c '.[] | (.[\"oops:hasNumberAffectedElements\"] | .[\"#text\"]), (.[\"oops:hasDescription\"] | .[\"#text\"])'" | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/\<br\>/g' > minor.txt
 
 # Put everything into one file
 echo "OOPS Summary: (always the occurrences amount and then the description) <br> Critical: <br> " `cat critical.txt` " <br>  <br> Important: <br> " `cat important.txt` " <br>  <br> Minor: <br> " `cat minor.txt` " <br>  <br> " > reports.txt
